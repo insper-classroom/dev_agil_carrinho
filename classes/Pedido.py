f09b6caf60a58439597efc3051adb5aa74deafd0
tiago.eem@gmail.com <Tiagoeem@users.noreply.github.com>
2022-08-18 13:23:12 -0300

    Commit inicial

---
+++ classes/Pedido.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  :
+# Created Date:
+# version ='1.0'
+# ---------------------------------------------------------------------------
+
+from classes.Endereco import Endereco
+from classes.PessoaFisica import PessoaFisica
+from classes.Carrinho import Carrinho
+import re
+
+# Modele e implemente a classe Pedido, lembrando que um pedido só pode ser gerado
+# com os dados de uma pessoa, dados de entrega, carrinho e pagamento
+
+class Pedido:
+    pass
+    
